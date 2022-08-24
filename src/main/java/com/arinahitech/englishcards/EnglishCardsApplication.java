090c2ab6d41a4c7b7ca1f2a80a7ab5683c895528
Arina Slizh <arina_slizh@epam.com>
2022-08-24 22:26:17 +0300

    Add simple API

--- src/main/java/com/arinahitech/englishcards/EnglishCardsApplication.java
+++ src/main/java/com/arinahitech/englishcards/EnglishCardsApplication.java
@@ -2,12 +2,23 @@ package com.arinahitech.englishcards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class EnglishCardsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EnglishCardsApplication.class, args);
+	}
+
+	@GetMapping
+	public List<String> hello() {
+		return Arrays.asList("Hello", "World");
 	}
 
 }
