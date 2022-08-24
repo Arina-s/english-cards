5958621a99adab432fb3b4ee76b16900c36425e8
Arina Slizh <arina_slizh@epam.com>
2022-08-24 23:43:38 +0300

    Add PostgreSQL connection

--- src/main/java/com/arinahitech/englishcards/modal/Card.java
+++ src/main/java/com/arinahitech/englishcards/modal/Card.java
@@ -4,13 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
 public class Card {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String phrase;
     private String translate;
