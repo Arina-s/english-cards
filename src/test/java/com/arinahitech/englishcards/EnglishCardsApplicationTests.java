764f2734bd695e55f73f8fb7c21de9e2c4324e74
Arina Slizh <arina_slizh@epam.com>
2022-08-24 18:42:02 +0300

    Initial commit

---
+++ src/test/java/com/arinahitech/englishcards/EnglishCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arinahitech.englishcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
