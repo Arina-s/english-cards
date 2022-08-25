0dd8c4d956d2577f7a91000d9e6f736c650ea323
Arina Slizh <arina_slizh@epam.com>
2022-08-25 13:59:01 +0300

    Add getCard, list with limit

--- src/main/java/com/arinahitech/englishcards/service/CardService.java
+++ src/main/java/com/arinahitech/englishcards/service/CardService.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CardService {
@@ -24,6 +25,10 @@ public class CardService {
 
     public List<Card> getCards() {
         return cardRepository.findAll();
+    }
+
+    public Card getCard(Long id) {
+        return cardRepository.findById(id).orElseThrow(() -> new CardNotFoundException(id));
     }
 
     public void addCard(Card card) {
@@ -46,7 +51,7 @@ public class CardService {
 
     @Transactional
     public Card updateCard(Long id, Card updateCard) {
-        Card card = cardRepository.findById(id).orElseThrow(() -> new CardNotFoundException(id));
+        Card card = getCard(id);
 
         if (isFieldSet(updateCard.getPhrase())) {
             card.setPhrase(updateCard.getPhrase());
@@ -62,6 +67,13 @@ public class CardService {
         }
 
         return cardRepository.save(card);
+    }
+
+    public List<Card> list(int limit) {
+        return cardRepository.findAll()
+                .stream()
+                .limit(limit)
+                .collect(Collectors.toList());
     }
 
     private boolean isFieldSet(String field) {
