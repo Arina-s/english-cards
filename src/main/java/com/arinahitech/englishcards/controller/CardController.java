c18e348c6ee0ace31710de8c6a31158eae665f35
Arina Slizh <arina_slizh@epam.com>
2022-08-27 12:14:39 +0300

    Add images, application.yml, Response

--- src/main/java/com/arinahitech/englishcards/controller/CardController.java
+++ src/main/java/com/arinahitech/englishcards/controller/CardController.java
@@ -1,11 +1,21 @@
 package com.arinahitech.englishcards.controller;
 
 import com.arinahitech.englishcards.modal.db.Card;
+import com.arinahitech.englishcards.modal.db.Response;
 import com.arinahitech.englishcards.service.CardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 
 @RestController
 @RequestMapping("api/v1/cards")
@@ -15,22 +25,62 @@ public class CardController {
     private final CardService cardService;
 
     @GetMapping
-    public List<Card> getCards() {
-        return cardService.getCards();
+    public ResponseEntity<Response> getCards() {
+        System.out.println("Working Directory = " + System.getProperty("user.dir") + "src/main/resources/static/img");
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .statusCode(HttpStatus.OK.value())
+                        .status(HttpStatus.OK)
+                        .message("Cards retrieved")
+                        .data(Map.of("cards", cardService.list(10)))
+                        .build()
+        );
     }
 
     @PostMapping
-    public Card newPhraseRegistration(@RequestBody Card card) {
-        return cardService.addCard(card);
+    public ResponseEntity<Response> newPhraseRegistration(@RequestBody @Valid Card card) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .statusCode(HttpStatus.CREATED.value())
+                        .status(HttpStatus.CREATED)
+                        .message("Card saved")
+                        .data(Map.of("card", cardService.addCard(card)))
+                        .build()
+        );
     }
 
     @PatchMapping("/{id}")
-    public Card updateCard(@PathVariable("id") Long id, @RequestBody Card card) {
-        return cardService.updateCard(id, card);
+    public ResponseEntity<Response> updateCard(@PathVariable("id") Long id, @RequestBody @Valid Card card) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .statusCode(HttpStatus.OK.value())
+                        .status(HttpStatus.OK)
+                        .message("Card updated")
+                        .data(Map.of("card", cardService.updateCard(id, card)))
+                        .build()
+        );
     }
 
     @DeleteMapping("/{id}")
-    public boolean deleteCard(@PathVariable("id") Long id) {
-        return cardService.deleteCard(id);
+    public ResponseEntity<Response> deleteCard(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .statusCode(HttpStatus.OK.value())
+                        .status(HttpStatus.OK)
+                        .message("Card deleted")
+                        .data(Map.of("delete", cardService.deleteCard(id)))
+                        .build()
+        );
+    }
+
+    @GetMapping(path = "/images/cardStatus/{id}", produces = IMAGE_PNG_VALUE)
+    public byte[] getStatusImage(@PathVariable("id") Long id) throws IOException {
+        return Files.readAllBytes(Paths.get(
+                System.getProperty("user.dir") + "/src/main/resources/static/images/cardstatus/" +
+                        cardService.getCard(id).getCardStatus().getImgName()));
     }
 }
--- src/main/java/com/arinahitech/englishcards/enums/CardStatus.java
+++ src/main/java/com/arinahitech/englishcards/enums/CardStatus.java
@@ -1,25 +1,17 @@
 package com.arinahitech.englishcards.enums;
 
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 public enum CardStatus {
-    CREATED("created.png"),
-    IN_STUDYING("in_studying.png"),
-    LEARNED("learned.png");
+    CREATED("created-icon.png"),
+    IN_STUDYING("in-studying-icon.png"),
+    LEARNED("learned-icon.png");
 
-    private String imgUrl;
     private String imgName;
 
     CardStatus(String imgName) {
         this.imgName = imgName;
-        this.imgUrl = ServletUriComponentsBuilder.fromCurrentContextPath().path("/status/image/" + imgName).toUriString();
     }
 
-    public void setImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl;
-    }
-
-    public String getImgUrl(String imgUrl) {
-        return this.imgUrl;
+    public String getImgName() {
+        return this.imgName;
     }
 }
